89d26481ad810d67bdc19ea3f9e7678631be62da
esvika <esvika@edu.riga.lv>
2025-05-10 23:11:20 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,56 +1,155 @@
-#include <iostream> // Lai strādātu ar ievadi un izvadi
-#include <vector> // Lai saglabātu iespējamos vārdus sarakstā
-#include <string> // Strādāšanai ar tekstiem
-#include <cstdlib> // Priekš random funkcijas
-#include <ctime> // Lai ģenerētu atšķirīgus nejaušus skaitļus
-#include <algorithm> // Lai iegūtu funkcijas, piemeram, all_of()
+#include <iostream> // Lai straadatu ar ievadi un izvadi
+#include <vector> // Lai saglabaatu iespeejamos vaardus sarakstaa
+#include <string> // Stradashanai ar tekstiem
+#include <cstdlib> // Prieks random funkcijas
+#include <ctime> // Lai genereetu atskirigus nejausus skaitlus
+#include <algorithm> // Lai iegutu funkcijas, piemeram, all_of()
 
 using namespace std;
 
-void displayGameIntro() { // Spēles intro
-    cout << "Laipni lūgts karatavas!" << endl;
-    cout << "Šajā spēlē tev ir jāuzmin slepenais vārds (angliski)!" << endl;
-    cout << "Ar katru nepareizu minējumu, tu zaudē dzīvību." << endl;
-    cout << "Veiksmi!" << endl;
-    cout << endl;
+
+
+// Parada sveicienu un noteikumus
+void speleIntro() {
+    cout << "Laipni lugts karatavas!" << endl;
+    cout << "Spele jaatmin slepens vards anglu valodaa." << endl;
+    cout << "Par katru nepareizu minejumu tiek zaudeta dziviba." << endl << endl;
 }
 
-string getRandomWord() {// Funkcija nejausi izvelas vardu
-    vector<string> randomVards = {"Function", "Programs", "Blueberry", "Windows", "Computer", "Happiness", "Rainbows", "Attention", "Starlight", "Refrigerator", "Conditioner"};
+
+
+// Atgriez vienu nejausi izveletu vardu no saraksta
+string sanemtVardu() {
+    vector<string> varduSaraksts = {
+        "Function", "Programs", "Blueberry", "Windows", "Computer",
+        "Happiness", "Rainbows", "Attention", "Starlight", "Refrigerator", "Conditioner"
+    };
     srand(time(0));
-    int randomNum = rand() % randomVards.size();
-    return randomVards[randomNum];
+    return varduSaraksts[rand() % varduSaraksts.size()];
 }
 
-void displayWord(const string &word, const vector<bool> &guessed) { // Funkcija aizvieto burtus ar _
-    for (size_t i = 0; i < word.size(); i++) {
-        if (guessed[i])
-            cout << word[i] << " ";
-        else
-            cout << "_ ";
+
+
+// Paraada vardu ar pasleptiem burtiem (_), iznemot jau atminetos
+void paraditVardu(const string& vards, const vector<bool>& uzminets) {
+    for (size_t i = 0; i < vards.size(); ++i) {
+        cout << (uzminets[i] ? vards[i] : '_') << " ";
     }
     cout << endl;
 }
-
-char getGuess() { // Lietotājs ievada minējumu
-    char guess;
-    cout << "Ievadi burtu: ";
-    cin >> guess;
-    return tolower(guess);
-}
-
-int chooseDifficulty() { // Izvelas grutibas limeni
-    int level;
+// Lietotajs izvelas grutibas limeni (1 - viegls, 2 - videjs, 3 - gruts)
+int izveletiesLimeni() {
+    int limenis;
     cout << "Izvelies grutibas limeni: (1) Viegls, (2) Videjs, (3) Gruts" << endl;
     cout << "Ievadi 1, 2 vai 3: ";
-    cin >> level;
-    return level;
+    cin >> limenis;
+    if (limenis < 1 || limenis > 3) {
+        cout << "Nederigs limenis, iestatits grutakais limenis" << endl;
+        return 3;
+    }
+    return limenis;
+}
+// Atgriez atbilstoso dzivibu skaitu pec izveleta limena
+int sanemtDzivibas(int limenis) {
+    if (limenis == 1) return 12;
+    if (limenis == 2) return 8;
+    return 6;
 }
 
-int getAttemptsForDifficulty(int level) {
-    switch (level) {
-        case 1: return 12;
-        case 2: return 8;
-        case 3: return 6;
-        default: return 6;
+
+
+
+
+
+
+// Galvenais speles cikls vienai spelei
+bool speletKolu(int dzivibas) {
+    string vards = sanemtVardu();
+    vector<bool> uzminets(vards.size(), false);
+    string minejumi;
+
+    while (dzivibas > 0) {
+        // Parada spelei aktualo vardu, dzivibas un minejumus
+        paraditVardu(vards, uzminets);
+        cout << "Atlikusas dzivibas: " << dzivibas << endl;
+        cout << "Mineetie burti: " << minejumi << endl;
+
+        string ievade;
+        cout << "Ievadi vienu burtu: ";
+        cin >> ievade;
+
+        // Parbauda vai ievade ir tikai viens alfabeta burts
+        if (ievade.size() != 1 || !isalpha(ievade[0])) {
+            cout << "Ievadi tikai vienu burtu, megini velreiz!" << endl;
+            continue;
+        }
+
+        char burts = ievade[0];
+
+        // Parbauda vai burts jau mineets
+        if (minejumi.find(burts) != string::npos) {
+            cout << "Sis burts jau ir mineets!" << endl;
+            continue;
+        }
+
+        minejumi += burts;
+        bool trapis = false;
+
+        // Parbauda vai burts ir slepenaja varda
+        for (size_t i = 0; i < vards.size(); ++i) {
+            if (vards[i] == burts || vards[i] == toupper(burts)) {
+                uzminets[i] = true;
+                trapis = true;
+            }
+        }
+        // Ja burts nav trapis, samazina dzivibu
+        if (!trapis) {
+            dzivibas--;
+            cout << "Burts nav varda!" << endl;
+        }
+
+        // Ja visi burti ir atmineti, spele uzvareta
+        if (all_of(uzminets.begin(), uzminets.end(), [](bool g) { return g; })) {
+            cout << "Apsveicam! Tu atmineji vardu: " << vards << endl;
+            return true;
+        }
     }
+    // Ja nav vairs dzivibu, spele zaudeta
+    cout << "Speles beigas! Slepenais vards bija: " << vards << endl;
+    return false;
+}
+
+
+
+
+
+
+
+// Cikls, kas atkartoti izpilda speli, kamer lietotajs to velas
+void speletSpele() {
+    int uzvaras = 0, zaudejumi = 0;
+    char velreiz = 'y';
+
+    while (velreiz == 'y' || velreiz == 'Y') {
+        int limenis = izveletiesLimeni();
+        int dzivibas = sanemtDzivibas(limenis);
+        bool uzvarets = speletKolu(dzivibas);
+
+        if (uzvarets) uzvaras++;
+        else zaudejumi++;
+
+        cout << "Uzvaras: " << uzvaras << " | Zaudejumi: " << zaudejumi << endl;
+        cout << "Spelet velreiz? (y/n): ";
+        cin >> velreiz;
+    }
+
+    cout << "\nPaldies par speeli!" << endl;
+    cout << "Gala statistika: Uzvaras: " << uzvaras << " | Zaudejumi: " << zaudejumi << endl;
+}
+
+// Programmas sakums
+int main() {
+    speleIntro();
+    speletSpele();
+    return 0;
+}
